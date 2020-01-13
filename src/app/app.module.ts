c7968cb56cad47fe5968fe73df899c47c1b296da
Anna Ambroziak <ambroziak_anna@wp.pl>
2020-01-13 15:03:15 +0100

    add filterEmptyPipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TodoAuthComponent } from './todo-auth/todo-auth.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { FilterEmptyPipe } from './pipes/filter-empty.pipe';
 
 @NgModule({
     declarations: [
         AppComponent,
         TodoAuthComponent,
-        TodoListComponent
+        TodoListComponent,
+        FilterEmptyPipe
     ],
     imports: [
         BrowserModule,
---
+++ src/app/pipes/filter-empty.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'filterEmpty'
+})
+export class FilterEmptyPipe implements PipeTransform {
+
+    transform(value: any[], ...args: any[]): any {
+        return value.filter(item => item);
+    }
+}
