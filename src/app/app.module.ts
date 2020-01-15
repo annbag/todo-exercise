a08a9f465dd09508051a1f5ca5d83b6c02b1ceba
Anna Ambroziak <ambroziak_anna@wp.pl>
2020-01-15 11:10:48 +0100

    add empty routingg file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { TodoAuthComponent } from './todo-auth/todo-auth.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { FilterEmptyPipe } from './pipes/filter-empty.pipe';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import { FilterEmptyPipe } from './pipes/filter-empty.pipe';
     imports: [
         BrowserModule,
         FormsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        AppRoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
